691da65352e086ed451168d99bcb86155f948180
OlgaVashurina <ovashurina@gmail.com>
2024-07-19 18:45:51 +0200

    Обновление CurrencyRepository

--- src/repository/CurrencyRepository.java
+++ src/repository/CurrencyRepository.java
@@ -3,6 +3,7 @@ package repository;
 import model.Currency;
 import model.Rate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,4 +14,34 @@ public class CurrencyRepository {
     Map<String, Currency>  currenciesMap;
     // Map для хранения истории курсов валют: ключ - код валюты, значение - список курсов (Rate) для данной валюты
     Map<String, List<Rate>> history;
+
+    public CurrencyRepository(Map<String, Rate> currenciesRate, Map<String, Currency> currenciesMap, Map<String, List<Rate>> history) {
+        this.currenciesRate = currenciesRate;
+        this.currenciesMap = currenciesMap;
+        this.history = history;
+    }
+
+    public Map<String, Rate> getCurrenciesRate() {
+        return currenciesRate;
+    }
+
+    public Map<String, Currency> getCurrenciesMap() {
+        return currenciesMap;
+    }
+
+    public Map<String, List<Rate>> getHistory() {
+        return history;
+    }
+
+    // добавления валюты
+    public void addCurrency(Currency currency) {
+        currenciesMap.put(currency.getCode(), currency);
+    }
+
+    // обновления текущего курса
+    public void updateRate(String currencyCode, Rate rate) {
+        currenciesRate.put(currencyCode, rate);
+        history.computeIfAbsent(currencyCode, k -> new ArrayList<>()).add(rate);
+    }
 }
+
