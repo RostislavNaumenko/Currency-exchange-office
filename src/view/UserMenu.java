1e1e28b0193e711581d55bb0c7b6276a60409f52
OlgaVashurina <ovashurina@gmail.com>
2024-07-23 18:58:38 +0200

    UserMenu

--- src/view/UserMenu.java
+++ src/view/UserMenu.java
@@ -1,25 +1,32 @@
 package view;
 
+import model.Account;
+import model.User;
+import service.AccountService;
 import service.TransactionService;
 import service.UserService;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class UserMenu {
     private final UserService userService;
     private final TransactionService transactionService;
+    private final AccountService accountService;
     private final Scanner scanner = new Scanner(System.in);
 
-    public UserMenu(UserService userService, TransactionService transactionService) {
+    public UserMenu(UserService userService, TransactionService transactionService, AccountService accountService) {
         this.userService = userService;
         this.transactionService = transactionService;
+        this.accountService = accountService;
     }
 
     public void showUserMenu() {
         while (true) {
             System.out.println("МЕНЮ ПОЛЬЗОВАТЕЛЯ");
             System.out.println("1 -> Информация о пользователе");
-            System.out.println("2 -> Переводы");
-            System.out.println("3 -> Просмотр аккаунта");
+            System.out.println("2 -> Просмотр аккаунта");
+            System.out.println("3 -> Переводы");
             System.out.println("4 -> История транзакций");
             System.out.println("0 -> Возврат в предыдущее меню");
 
@@ -39,13 +46,12 @@ public class UserMenu {
             case 1:
                 System.out.println("\nВаша информация: \n");
                 System.out.println(userService.getActiveUser());
-                waitRead();
                 break;
             case 2:
-                showTransferMenu();
+                viewAccount();
                 break;
             case 3:
-                viewAccount();
+                showTransferMenu();
                 break;
             case 4:
                 showTransactionHistoryMenu();
@@ -90,17 +96,86 @@ public class UserMenu {
 
     private void makeInternalTransfer() {
         System.out.println("Перевод между своими счетами");
-        waitRead();
+
+        User activeUser = userService.getActiveUser();
+        List<Account> accounts = accountService.getAccountsByUserId(activeUser.getId());
+
+        System.out.println("Ваши аккаунты:");
+        for (int i = 0; i < accounts.size(); i++) {
+            System.out.println(i + " -> " + accounts.get(i));
+        }
+
+        System.out.println("Выберите аккаунт для списания средств:");
+        int fromIndex = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Выберите аккаунт для зачисления средств:");
+        int toIndex = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Введите сумму перевода:");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+
+        Account fromAccount = accounts.get(fromIndex);
+        Account toAccount = accounts.get(toIndex);
+
     }
 
     private void makeExternalTransfer() {
         System.out.println("Перевод на другой аккаунт");
-        waitRead();
+
+        User activeUser = userService.getActiveUser();
+        List<Account> accounts = accountService.getAccountsByUserId(activeUser.getId());
+
+        System.out.println("Ваши аккаунты:");
+        for (int i = 0; i < accounts.size(); i++) {
+            System.out.println(i + " -> " + accounts.get(i));
+        }
+
+        System.out.println("Выберите аккаунт для списания средств:");
+        int fromIndex = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Введите email пользователя, которому вы хотите перевести средства:");
+        String email = scanner.nextLine();
+        User recipient = userService.getUserByEmail(email);
+
+        if (recipient == null) {
+            System.out.println("Пользователь с таким email не найден.");
+            return;
+        }
+
+        List<Account> recipientAccounts = accountService.getAccountsByUserId(recipient.getId());
+        System.out.println("Аккаунты получателя:");
+        for (int i = 0; i < recipientAccounts.size(); i++) {
+            System.out.println(i + " -> " + recipientAccounts.get(i));
+        }
+
+        System.out.println("Выберите аккаунт для зачисления средств:");
+        int toIndex = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Введите сумму перевода:");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+
+        Account fromAccount = accounts.get(fromIndex);
+        Account toAccount = recipientAccounts.get(toIndex);
+
     }
 
     private void viewAccount() {
         System.out.println("Просмотр аккаунта");
-        waitRead();
+
+        User activeUser = userService.getActiveUser();
+        List<Account> accounts = accountService.getAccountsByUserId(activeUser.getId());
+
+        System.out.println("Ваши аккаунты:");
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+
     }
 
     private void showTransactionHistoryMenu() {
@@ -119,29 +194,13 @@ public class UserMenu {
         switch (input) {
             case 1:
                 //передать User, и вызвать метод с accountService
-                showAllUserTransactions();
                 break;
             case 2:
                 //вывести все аккаунты, передать id  метод с accountService
-                showAccountTransactions();
                 break;
             default:
                 System.out.println("Некорректный выбор\n");
         }
     }
-    //Удалить
-    private void showAllUserTransactions() {
-        System.out.println("Все транзакции пользователя");
-        waitRead();
-    }
 
-    private void showAccountTransactions() {
-        System.out.println("Транзакции по аккаунту");
-        waitRead();
-    }
-
-    public void waitRead() {
-        System.out.println("\nДля продолжения нажмите Enter ");
-        scanner.nextLine();
-    }
 }
